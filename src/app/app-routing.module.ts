480dfe6904c2f240519552b43163b5725c440bf7
marquescami <camilamsc@hotmail.com>
2022-06-08 10:21:53 -0300

    cards produtos consumindo de uma API

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './component/cart/cart.component';
+import { ProductsComponent } from './component/products/products.component';
 
 const routes: Routes = [];
 
